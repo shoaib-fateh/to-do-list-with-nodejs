a3c1854373661e8651a7bdeef394316452b2efb9
Shoaib Fateh <115332099+shoaib-fateh@users.noreply.github.com>
2022-10-26 03:18:53 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+    // jshint esversion:6
+    const express = require("express");
+    const bodyParser = require("body-parser");
+
+    var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+    const app = express();
+
+    app.set('view engine', 'ejs');
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(express.static("public"))
+
+
+    app.get("/", function(req, res){
+        var today = new Date();
+        
+        var options ={
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+        };
+
+        var day = today.toLocaleDateString("en-US", options);
+
+        res.render("list", {
+            kindOfDay: day,
+            newListItems: items
+        });
+    });
+    app.post("/", function(req, res){
+        item = req.body.newItem;
+        items.push(item)
+        res.redirect("/");
+    } );
+    
+    app.listen(3000, function(){
+        console.log("Server started on port 3000");
+    })
